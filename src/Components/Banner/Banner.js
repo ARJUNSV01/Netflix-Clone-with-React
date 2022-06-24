84a8a0a4fd5daf59b54edaf12743198e799e96e9
ARJUNSV01 <arjunsv01@gmail.com>
2022-06-24 23:14:31 +0530

    Basic JSX and styling completed

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className='content'>
+            <h1 className='title'>Movie name</h1>
+            <div className='banner_buttons'>
+                <button className='button'>Play</button>
+                <button className='button'>My list</button>
+            </div>
+            <h1 className='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h1>
+        </div>
+       <div className="fade_bottom"></div>
+    </div>
+  )
+}
+
+export default Banner
